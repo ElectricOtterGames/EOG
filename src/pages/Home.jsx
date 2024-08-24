57676d52c5fd695561067d4d438d788ad79a1dc5
Sabrina Jackson <spjacks2@ncsu.edu>
2024-08-24 19:51:11 -0400

    link routing attempt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
             <div id="page" class="mt-5">
                 <h3 class="mb-3">Welcome</h3>
                 <p>This is my space to word vomit about my hobbies.</p>
-                <p><strong><a as={Link} to="/cassie" className="link">Proceed if you dare.</a></strong> Or don't. Up to you.</p>
+                <p><strong><Link to="/cassie" className="link">Proceed if you dare.</Link></strong> Or don't. Up to you.</p>
             </div>
         </>
     )
