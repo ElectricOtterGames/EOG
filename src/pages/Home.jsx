983ec05c658d45554692756db64e9409b03ef267
Sabrina Jackson <spjacks2@ncsu.edu>
2024-08-24 19:49:11 -0400

    link routing attempt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+
 function Home() {
     return (
         <>
             <div id="page" class="mt-5">
                 <h3 class="mb-3">Welcome</h3>
                 <p>This is my space to word vomit about my hobbies.</p>
-                <p><strong><a href="/cassie" className="link">Proceed if you dare.</a></strong> Or don't. Up to you.</p>
+                <p><strong><a as={Link} to="/cassie" className="link">Proceed if you dare.</a></strong> Or don't. Up to you.</p>
             </div>
         </>
     )
